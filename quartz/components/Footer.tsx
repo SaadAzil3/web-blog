bb237456716d608570291b10647ca1ea3918d949
SaadAzil3 <azilsaad06@gmail.com>
2025-03-23 20:07:26 +0100

    Quartz sync: Mar 23, 2025, 8:07 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -10,12 +10,16 @@ interface Options {
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+    const links = {
+    "LinkedIn": "https://www.linkedin.com/in/saadazil/",
+    "GitHub": "https://github.com/SaadAzil3",
+    // Add more links as needed
+  }
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="">SAAD AZIL{version}</a> © {year}
+          <a href="">SAAD AZIL</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
