2b3f6ccb625e3c29db4ab3665fb613bdc460d7c3
SaadAzil3 <azilsaad06@gmail.com>
2025-03-23 18:37:18 +0100

    Quartz sync: Mar 23, 2025, 6:37 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -3,30 +3,31 @@ import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
-const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-  const year = new Date().getFullYear()
-  const links = {
-    "LinkedIn": "https://www.linkedin.com/in/saadazil/",
-    "GitHub": "https://github.com/SaadAzil3",
-    // Add more links as needed
-  }
-  return (
-    <footer class={`${displayClass ?? ""}`}>
-      <p>
-        {i18n(cfg.locale).components.footer.createdWith}{" "}
-        <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-      </p>
-      <ul>
-        {Object.entries(links).map(([text, link]) => (
-          <li>
-            <a href={link}>{text}</a>
-          </li>
-        ))}
-      </ul>
-    </footer>
-  )
+interface Options {
+  links: Record<string, string>
 }
 
-Footer.css = style
-export default Footer satisfies QuartzComponentConstructor
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer class={${displayClass ?? ""}}>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="">SAAD AZIL</a> © {year}
+        </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    )
+  }
 
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
